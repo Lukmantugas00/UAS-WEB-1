7257d989f2b26209599c5b54755fc9c74745c3d6
unknown <lukmantugas00@gmail.com>
2025-01-16 12:59:50 +0800

    first commit

---
+++ AkhmadLukmanHAkim_2310020007_UTS_WEB1/script.js
@@ -0,0 +1,8 @@
+// Example JavaScript for interactivity
+
+document.addEventListener('DOMContentLoaded', () => {
+    const changeProfileButton = document.querySelector('button.bg-gray-300');
+    changeProfileButton.addEventListener('click', () => {
+        alert('Change profile functionality not implemented yet.');
+    });
+});
